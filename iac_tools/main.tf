4ad408979b818b55b14c9183a826f58329cb9a5e
bortolo <andrea.bortolossi89@gmail.com>
2022-12-19 14:34:40 +0100

    Ec2 with terraform working

--- iac_tools/main.tf
+++ iac_tools/main.tf
@@ -1,14 +1,15 @@
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "vpc-prova"
-  cidr = "10.0.0.0/16"
+  name = "vpc-iac"
+  cidr = var.network_structure["cidr"][0]
 
-  azs             = ["${var.aws_region}a"]
-  public_subnets = ["10.0.1.0/24"]
+  azs             = var.network_structure["azs"]
+  public_subnets  = var.network_structure["public"]
+  private_subnets = var.network_structure["private"]
 
-  enable_ipv6 = true
-
+  enable_ipv6 = false
   enable_nat_gateway = false
   single_nat_gateway = false
 
@@ -17,6 +18,64 @@ module "vpc" {
   }
 
   vpc_tags = {
-    Name = "vpc-name"
+    Name = "vpc-iac"
   }
+}
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+
+  name        = "security-group-iac-tool"
+  description = "Security group for execa IaC tool instance"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["ssh-tcp","all-icmp"]
+  egress_rules        = ["all-all"]
+
+}
+
+resource "aws_key_pair" "this" {
+  key_name   = "ec2-key-pair"
+  public_key = file("./resources/id_rsa.pub")
+}
+
+module "ec2" {
+    source = "terraform-aws-modules/ec2-instance/aws"
+
+    name                        = "ec2-iac-tool"
+    ami                         = "ami-0a261c0e5f51090b1"
+    instance_type               = "t2.micro"
+    availability_zone           = element(module.vpc.azs, 0)
+    subnet_id                   = element(module.vpc.public_subnets, 0)
+    vpc_security_group_ids      = [module.security_group.security_group_id]
+    associate_public_ip_address = true
+    key_name                    = "ec2-key-pair"
+
+  user_data_base64            = base64encode(file("./resources/userdata.txt"))
+  user_data_replace_on_change = true
+
+  enable_volume_tags = false
+  root_block_device = [
+    {
+      encrypted   = true
+      volume_type = "gp3"
+      throughput  = 200
+      volume_size = 50
+      tags = {
+        Name = "my-root-block"
+      }
+    },
+  ]
+
+  ebs_block_device = [
+    {
+      device_name = "/dev/sdf"
+      volume_type = "gp3"
+      volume_size = 5
+      throughput  = 200
+      encrypted   = false
+    }
+  ]
+
 }
