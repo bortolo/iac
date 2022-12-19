b1bd5a1af37db134bbe1ec028278875a8588788c
bortolo <andrea.bortolossi89@gmail.com>
2022-12-19 17:22:56 +0100

    Dynamic inventory installed

--- iac_tools/main.tf
+++ iac_tools/main.tf
@@ -1,3 +1,9 @@
+locals {
+  tags = {
+    Owner       = "andrea.bortolossi"
+    Environment = "infrastructure-tools"
+  }
+}
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -9,13 +15,11 @@ module "vpc" {
   public_subnets  = var.network_structure["public"]
   private_subnets = var.network_structure["private"]
 
-  enable_ipv6 = false
+  enable_ipv6        = false
   enable_nat_gateway = false
   single_nat_gateway = false
 
-  public_subnet_tags = {
-    Name = "overridden-name-public"
-  }
+  public_subnet_tags = local.tags
 
   vpc_tags = {
     Name = "vpc-iac"
@@ -23,14 +27,14 @@ module "vpc" {
 }
 
 module "security_group" {
-  source  = "terraform-aws-modules/security-group/aws"
+  source = "terraform-aws-modules/security-group/aws"
 
   name        = "security-group-iac-tool"
   description = "Security group for execa IaC tool instance"
   vpc_id      = module.vpc.vpc_id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["ssh-tcp","all-icmp"]
+  ingress_rules       = ["ssh-tcp", "all-icmp"]
   egress_rules        = ["all-all"]
 
 }
@@ -41,16 +45,22 @@ resource "aws_key_pair" "this" {
 }
 
 module "ec2" {
-    source = "terraform-aws-modules/ec2-instance/aws"
+  source = "terraform-aws-modules/ec2-instance/aws"
 
-    name                        = "ec2-iac-tool"
-    ami                         = "ami-0a261c0e5f51090b1"
-    instance_type               = "t2.micro"
-    availability_zone           = element(module.vpc.azs, 0)
-    subnet_id                   = element(module.vpc.public_subnets, 0)
-    vpc_security_group_ids      = [module.security_group.security_group_id]
-    associate_public_ip_address = true
-    key_name                    = "ec2-key-pair"
+  name                        = "ec2-iac-tool"
+  ami                         = "ami-0a261c0e5f51090b1"
+  instance_type               = "t2.micro"
+  availability_zone           = element(module.vpc.azs, 0)
+  subnet_id                   = element(module.vpc.public_subnets, 0)
+  vpc_security_group_ids      = [module.security_group.security_group_id]
+  associate_public_ip_address = true
+  key_name                    = "ec2-key-pair"
+
+  create_iam_instance_profile = true
+  iam_role_description        = "IAM role for EC2 instance"
+  iam_role_policies = {
+    AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
+  }
 
   user_data_base64            = base64encode(file("./resources/userdata.txt"))
   user_data_replace_on_change = true
@@ -77,5 +87,7 @@ module "ec2" {
       encrypted   = false
     }
   ]
+
+    tags = local.tags
 
 }
